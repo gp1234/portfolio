b2057ad94e2e7c5364f51ca52e735bea79b399ab
Giovanni Poveda <giovannipoveda@Giovannis-MacBook-Pro.local>
2023-10-24 00:03:15 -0500

    chore: add three library and folder arrangement

---
+++ src/js/animations/3DAnimations.ts
@@ -0,0 +1,6 @@
+import Three from "three";
+document.addEventListener("DOMContentLoaded", loadThree);
+
+function loadThree() {
+  console.log(Three);
+}
--- src/js/main.ts
+++ src/js/main.ts
@@ -1,21 +1,5 @@
 import "../scss/main.scss";
-import { gsap } from "gsap";
 
-const attributes = document.querySelectorAll(".attributes-elements");
-
-const hero = gsap.to(".word", { opacity: 1, duration: 1, stagger: 1 });
-const mainTimeline = gsap.timeline();
-const attributesTimeline = gsap.timeline({ repeat: -1 });
-
-mainTimeline.add(hero);
-for (let i = 0; i < attributes.length; i++) {
-  const individualWordTimeline = gsap.timeline();
-  individualWordTimeline.from(attributes[i], { y: "-100%", opacity: 0 });
-  individualWordTimeline.to(attributes[i], {
-    y: "100%",
-    opacity: 0,
-  });
-  individualWordTimeline.duration(2);
-  attributesTimeline.add(individualWordTimeline);
-}
-mainTimeline.add(attributesTimeline);
+//animations
+import "./animations/2DAnimations";
+import "./animations/3DAnimations";

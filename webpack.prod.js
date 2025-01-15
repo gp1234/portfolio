0fac83bac1ff355ee1b43f576ceea165829ef366
Giovanni Poveda <giovannipoveda51@gmail.com>
2025-01-15 12:47:58 +0100

    feat(main.ts): add a display none until the js is loaded

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,25 +1,6 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
-    mode: "production",
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i, // Extract CSS into separate files
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-            },
-        ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "[name].[contenthash].css", // Cache-busting with content hash
-        }),
-    ],
-    optimization: {
-        splitChunks: {
-            chunks: "all", // Code splitting
-        },
-    },
-});
+    mode: 'production'
+})
